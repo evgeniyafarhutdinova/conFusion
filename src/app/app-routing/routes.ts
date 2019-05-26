67c818427f63e129710b8ebe36beebe89a70b7e2
farkhutdinova <evgeniyafarhutdinova@gmail.com>
2019-05-26 18:52:44 +0300

    SPA Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 
 import { MenuComponent } from '../menu/menu.component';
 import { HomeComponent } from '../home/home.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'contactus',     component: ContactComponent }
 ];
